b758afa8e5821bcd2ed2e0eff3149acdd49cf694
i <isomorphisms@sdf.org>
2026-09-22 15:48:22 -0700

    Reconstruct symbolic chord notation in ChordAdded.java

--- app/src/main/java/org/hollowbamboo/chordreader2/chords/ChordAdded.java
+++ app/src/main/java/org/hollowbamboo/chordreader2/chords/ChordAdded.java
@@ -1,59 +1,56 @@
 package org.hollowbamboo.chordreader2.chords;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
-/**
- * Enum for add9, add11, power chords, etc.
- * @author nolan
- *
- */
+/** Added tones and power chords. The first alias is the canonical printed form. */
 public enum ChordAdded {
 
-	Add6 (Arrays.asList("add6")),
-	Add9 (Arrays.asList("add9", "2")),
-	Add11 (Arrays.asList("add11", "4")),
-	Augmented9 (Arrays.asList("aug9","+9")),
-	AugmentedMajor9 (Arrays.asList("augmaj9","+M9")),
-	Major6 (Arrays.asList("6","maj6","major6", "M6")),
-	Major6Flat5 (Arrays.asList("6b5")),
-	SixNine (Arrays.asList("6/9","6add9","6/add9","69")),
-	NineFlat5 (Arrays.asList("9b5")),
-	NineSharp5 (Arrays.asList("9#5")),
-	PowerChord (Arrays.asList("5")), // duh duh DUH, duh duh DUH-duh, duh duh DUH, duh duh ((c) Deep Purple)
-	;
-	
-	private final List<String> aliases;
-	
-	ChordAdded (List<String> aliases) {
-		this.aliases = aliases;
-	}
-	
-	public List<String> getAliases() {
-		return aliases;
-	}
-	
-	public static List<String> getAllAliases() {
-		List<String> result = new ArrayList<String>();
-		
-		for (org.hollowbamboo.chordreader2.chords.ChordAdded chordAdded : values()) {
-			result.addAll(chordAdded.aliases);
-		}
-		
-		return result;
-	}
-	
-	private static final Map<String, org.hollowbamboo.chordreader2.chords.ChordAdded> lookupMap = new HashMap<String, org.hollowbamboo.chordreader2.chords.ChordAdded>();
-	
-	static {
-		for (org.hollowbamboo.chordreader2.chords.ChordAdded value : values()) {
-			for (String alias : value.aliases) {
-				lookupMap.put(alias.toLowerCase(), value);
-			}
-		}
-	}
-	
-	public static org.hollowbamboo.chordreader2.chords.ChordAdded findByAlias(String alias) {
-		return lookupMap.get(alias.toLowerCase());
-	}
+    Add6(Arrays.asList("add6")),
+    Add9(Arrays.asList("add9", "2")),
+    Add11(Arrays.asList("add11", "4")),
+    Augmented9(Arrays.asList("+9", "aug9")),
+    AugmentedMajor9(Arrays.asList("+M9", "augmaj9")),
+    Major6(Arrays.asList("6", "maj6", "major6", "M6")),
+    Major6Flat5(Arrays.asList("6♭5", "6b5")),
+    SixNine(Arrays.asList("6/9", "6add9", "6/add9", "69")),
+    NineFlat5(Arrays.asList("9♭5", "9b5")),
+    NineSharp5(Arrays.asList("9♯5", "9#5")),
+    PowerChord(Arrays.asList("5"));
 
+    private final List<String> aliases;
+
+    ChordAdded(List<String> aliases) {
+        this.aliases = aliases;
+    }
+
+    public List<String> getAliases() {
+        return aliases;
+    }
+
+    public static List<String> getAllAliases() {
+        List<String> result = new ArrayList<>();
+        for (ChordAdded added : values()) {
+            result.addAll(added.aliases);
+        }
+        return result;
+    }
+
+    private static final Map<String, ChordAdded> lookupMap = new HashMap<>();
+
+    static {
+        for (ChordAdded added : values()) {
+            for (String alias : added.aliases) {
+                lookupMap.put(alias.toLowerCase(Locale.ROOT), added);
+            }
+        }
+    }
+
+    public static ChordAdded findByAlias(String alias) {
+        return lookupMap.get(alias.toLowerCase(Locale.ROOT));
+    }
 }
